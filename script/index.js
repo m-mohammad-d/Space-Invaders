169e3ecd01502a902f3534dd6c1aa9de76da565f
silvershade1 <silvershade1000@gmail.com>
2025-01-10 19:55:20 +0330

    feat : add defaultSettings

--- script/index.js
+++ script/index.js
@@ -1,3 +1,17 @@
 import { Game } from "./Game.js";
+import { loadSettings } from "./utils.js";
+const setting = loadSettings();
+console.log(setting);
 
+if (!setting) {
+  const defaultSettings = {
+    volume: 50,
+    difficulty: "medium",
+    enemyGenerateSpeed: 2000,
+    enemyMoveSpeed: 2,
+    scorePerEnemy: 2,
+  };
+
+  localStorage.setItem("gameSettings", JSON.stringify(defaultSettings));
+}
 const game = new Game("canvas");
